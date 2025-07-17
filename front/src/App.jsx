7431edcaace6b96cb62ec5dff7eb344f25daf69f
kubanych030299@gmail.com <kubanych030299@gmail.com>
2025-07-17 12:35:44 -0400

    [front] added animation in HomeSection added scroll-reveal

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import NavBar from './components/Navbar/NavBar';
 import Contacts from './sections/Contacts/Contacts';
 import Footer from './sections/Footer/Footer';
@@ -5,8 +6,22 @@ import HomeSection from './sections/Home/HomeSection';
 import NewEvents from './sections/NewEvents/NewEvents';
 import NewProducts from './sections/NewProducts/NewProducts';
 import OurProducts from './sections/OurProducts/OurProducts';
+import scrollReveal from 'scrollReveal';
 
 const App = () => {
+  useEffect(() => {
+    const registerAnimation = () => {
+      const sr = scrollReveal({
+        origin: 'bottom',
+        distance: '20px',
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(`.navbar, .homeSection,.ourProducts, .NewProducts, .NewEvents, .Contacts, .footer`, { interval: 500 });
+    };
+    registerAnimation();
+  }, []);
+
   return (
     <>
       <div className="app-container">
